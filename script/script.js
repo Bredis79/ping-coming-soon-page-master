d37e09710a1cb37639ad7878f1f8dd7cc273a3d0
Bredis <77401763+Bredis79@users.noreply.github.com>
2022-09-29 05:07:03 +0200

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,47 @@
+const formInput = document.querySelector(".input")
+const inputEmail = document.querySelector("#email")
+const btn = document.querySelector("#btn")
+const errorMobile = document.querySelector(".errorMobile")
+const errorDesktop = document.querySelector(".errorDesktop")
+
+let paragraph = document.createElement("p")
+paragraph = "Please provide a valid email address"
+
+
+// email validation
+const emailValid = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+btn.addEventListener("click", function(event){
+    event.preventDefault()
+       
+// condition for resolutio greater than 700px 
+   if (event.view.window.innerWidth > 700){
+
+        if (event.path[1][0].value.match(emailValid)){
+               
+            } else {
+                inputEmail.style.border = "2px solid red"
+                formInput.style.marginBottom = "27px"
+                errorDesktop.classList.add("visibleDesktop")
+                errorDesktop.append(paragraph)
+                paragraph = ""
+            }
+
+// condition for resolution smaller than 700px
+   } else {
+        if (event.path[1][0].value.match(emailValid)){
+            
+        } else {
+            inputEmail.style.border = "2px solid red"
+            errorMobile.classList.add("visibleMobile")
+            errorMobile.append(paragraph)
+            paragraph = ""
+        }
+
+   }
+// form reset after click on submit btn and sending an email   
+   formInput.reset()
+   
+   
+   
+})
